a0a9d0ef46a3043bd1541802d4a17bcd7de47a74
ddangme <ssilver121@naver.com>
2024-10-29 18:19:36 +0900

    feat: 타임라인 탭의 타임라인 카드 노출 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import {Text, View, StyleSheet} from "react-native";
+import {View, StyleSheet, ScrollView} from "react-native";
 import React, {useState} from "react";
 import CustomCalendar from "@/app/components/timeline/CustomCalendar";
 import FloatingActionButton from "@/app/components/community/FloatingActionButton";
 import {useFocusEffect} from "@react-navigation/native";
 import TimelineWriteModal from "@/app/components/timeline/TimelineWriteModal";
+import TimeLineCard from "@/app/components/common/TimeLineCard";
 
 export default function Index() {
     const [isModalVisible, setIsModalVisible] = useState(false);
@@ -22,6 +23,32 @@ export default function Index() {
     ];
 
     // TODO: 타임라인 정보 리스트 API 호출 후 연결
+    const dummyTimelinePosts = [
+        {
+            id: 1,
+            title: "손상원 클라이밍 판교점",
+            subtitle: "엄청 다이나믹한 암장이었다 (사실 이지 브로)",
+            date: "2024. 10. 04.",
+            imageUrl: require("@/assets/images/climbing.png"),
+            color: '#735BF2'
+        },
+        {
+            id: 2,
+            title: "손상원 클라이밍 판교점",
+            subtitle: "오늘은 좀 힘들었다.",
+            date: "2024. 10. 05.",
+            imageUrl: require("@/assets/images/climbing.png"),
+            color: '#E31A1A'
+        },
+        {
+            id: 3,
+            title: "손상원 클라이밍 분당점",
+            subtitle: "오늘은 아쉬웠다.",
+            date: "2024. 10. 15.",
+            imageUrl: require("@/assets/images/climbing.png"),
+            color: '#E31A1A'
+        },
+    ];
 
 
     const activeMarkedDates = activeDates.reduce((acc, date) => ({
@@ -38,7 +65,14 @@ export default function Index() {
 
     return (
         <View style={styles.container}>
-            <CustomCalendar markedDates={activeMarkedDates} />
+            <ScrollView style={styles.scrollView}>
+                <CustomCalendar markedDates={activeMarkedDates} />
+                <View style={styles.cardsContainer}>
+                    {dummyTimelinePosts.map((post) => (
+                        <TimeLineCard key={post.id} post={post} />
+                    ))}
+                </View>
+            </ScrollView>
             <FloatingActionButton onPress={() => setIsModalVisible(true)} />
             <TimelineWriteModal
                 visible={isModalVisible}
@@ -53,5 +87,11 @@ const styles = StyleSheet.create({
         paddingTop: 0,
         flex: 1,
         backgroundColor: 'white',
+    },
+    scrollView: {
+        flex: 1,
+    },
+    cardsContainer: {
+        padding: 16,
     },
 });
