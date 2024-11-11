ddaf23a235934737fc78b81a5f6e1fcdf9a1541e
ddangme <ssilver121@naver.com>
2024-11-11 17:40:34 +0900

    style: 불필요한 코드 삭제

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,6 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Image } from "react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import {Ionicons} from "@expo/vector-icons";
-
 export default function TabLayout() {
     return (
         <Tabs
